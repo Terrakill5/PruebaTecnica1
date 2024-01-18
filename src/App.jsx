335326f3f624fd30b0acdb46cf1cdfd504ae27cc
JBriceno <josealbertino4@gmail.com>
2024-01-18 16:45:15 -0500

    Agregado filtrar por genero, queda bug por eliminacion de la lista de lectura

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 //import { useState } from 'react'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import "./App.css";
 import * as data from './books.json';
 
 function App() {
-   let [seleccionados, setSeleccionados] = useState([])
-   const [readingList, setReadingList] = useState([])
+
+  const [seleccionados, setSeleccionados] = useState([])
+  const [readingList, setReadingList] = useState([])
   
+  useEffect(() => {
+    
+    setSeleccionados([].concat(data.library));
+  }, []);
+
+  useEffect(() => {
+   console.log("tamaño seleccionados", seleccionados.length)
+   console.log("tamaño readingList", readingList.length)
+  }, [seleccionados,readingList]);
   const res = data.library;
-  console.log(res);
+  
+  
 
   // const dialog = useRef(null)
 
   const handleAddBook = (book) => {
-    const filteredBooks = res.filter(
+    const filteredBooks = seleccionados.filter(
       (item) => item.book.ISBN !== book.ISBN
     )
     setSeleccionados([].concat(filteredBooks));
@@ -24,32 +35,97 @@ function App() {
   }
 
   const handleRemoveBook = (book) => {
-    console.log(readingList)
+    //console.log(readingList)
     console.log(book);
     const newReadingList = readingList.filter(
       (item) => item.ISBN !== book.ISBN
     )
+    //console.log(newReadingList);
     setReadingList([].concat(newReadingList))
-    setSeleccionados(seleccionados.concat(book))
-    console.log(readingList);
-    console.log(seleccionados);
+    handleSeleccionadosFilter();
+    
   }
 
-  /* const handleRemoveBook = (book) => {
-    const newReadingList = readingList.filter(
-      (item) => item.book.ISBN !== book.book.ISBN
-    )
-    setReadingList(newReadingList)
-    setBooks([...books, book])
-  } */
-  // const listaLectura = [];
+  const handleSeleccionadosFilter = () => {
+    if(readingList.length === 0) {
+      setSeleccionados([].concat(data.library))
+    }
+    else {
+      const filteredBooks = res.filter(
+      (item) => {
+        let comparacion = true;
+        let index = 0;
+        while(index < readingList.length && comparacion === true){
+          comparacion = item.book.ISBN !== readingList[index].ISBN
+          index++;
+        }
+        return comparacion;
+      }
+      ) 
+    console.log("actualizar luego de borrar",filteredBooks);
+    setSeleccionados([].concat(filteredBooks));
+    }
+    
+  }
+
+  const handleChangeGenre = (e) => {
+    console.log(e.target.value);
+    const evento = parseInt(e.target.value);
+    let filteredBooks;
+    
+    switch(evento) {
+      case 1:
+        handleSeleccionadosFilter();
+      break;
+
+      case 2:
+        filteredBooks = res.filter(
+          (item) => item.book.genre === "Terror"
+        )
+        setSeleccionados([].concat(filteredBooks));
+        console.log(seleccionados);
+
+      break;
+      case 3:
+        filteredBooks = res.filter(
+          (item) => item.book.genre === "Ciencia ficción"
+        )
+        setSeleccionados([].concat(filteredBooks));
+        console.log(seleccionados);
+
+      break;
+
+      case 4:
+        filteredBooks = res.filter(
+          (item) => item.book.genre === "Zombies"
+        )
+        setSeleccionados([].concat(filteredBooks));
+        console.log(seleccionados);
+
+      break;
+
+      case 5:
+        filteredBooks = res.filter(
+          (item) => item.book.genre === "Fantasía"
+        )
+        setSeleccionados([].concat(filteredBooks));
+        console.log(seleccionados);
+
+      break;
+
+      default:
+        console.log("por aqui no debio ir");
+
+      break;
+    }
+  }
   
   return (
     <>
       <main>
         <div className="disponibles">
-          <h3>{res.length} libros disponibles</h3>
-          <h4>2 en la lista de lectura</h4>
+          <h3>{seleccionados.length} libros disponibles</h3>
+          {readingList.length !== 0 && <h4>{readingList.length} en la lista de lectura</h4>}
           <div className="Filtro">
             <div className="Filtro-pagina">
               <h5>Filtrar por páginas</h5>
@@ -57,11 +133,19 @@ function App() {
             </div>
             <div className="Filtro-genero">
               <h5>Filtrar por género</h5>
-              <div></div>
+              <div>
+                
+                <select name="genero" onChange={handleChangeGenre}> 
+                <option value="1" defaultChecked>Todas </option>
+                <option value="2">Terror</option>
+                <option value="3">Ciencia Ficcion</option>
+                <option value="4">Zombies</option>
+                </select> 
+                  </div>
             </div>
           </div>
           <div className="grid">
-            {res && res.map((index) => (
+            {seleccionados && seleccionados.map((index) => (
               <div key={index.book.ISBN}><img className="cover-img" src={index.book.cover} width="170px" onClick={() => handleAddBook(index.book)} /></div>
             ))}
             <div className=""></div>
